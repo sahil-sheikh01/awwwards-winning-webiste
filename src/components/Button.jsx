9acf950923159cd9be8a949dec4198b60edc5b1b
Sahil Sheikh <you@example.com>
2026-09-17 15:17:33 +0530

    fix button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,11 +3,9 @@ const Button = ({ title, id, rightIcon, leftIcon, containerClass }) => {
   return (
     <button id={id} className={`group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black ${containerClass}`}>
       {leftIcon}
-      <span className="relative incline-flex overflow-hidden font-general text-xs uppercase">
-        <div>
-          {title}
-          {rightIcon}
-        </div>
+      <span className="relative inline-flex items-center justify-center gap-1 overflow-hidden font-general text-xs uppercase">
+        {title}
+        {rightIcon}
       </span>
     </button>
   )
