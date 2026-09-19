77f8c620d17bc758e0217483967e49dd39007ea0
Sahil Sheikh <you@example.com>
2026-09-19 17:09:16 +0530

    fix bento tilt

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -64,7 +64,7 @@ const Features = () => {
                     <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">Immerse yourself in a rich and ever-expanding universe where a vibrant array of products converge into an interconnected overlay experience on your world.</p>
                 </div>
 
-                <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+                <BentoTilt className="bento-tilt_1 border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
                     <BentoCard
                         src="videos/feature-1.mp4"
                         title={<>radia<b>n</b>t</>}
